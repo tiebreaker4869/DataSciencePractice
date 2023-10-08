8974ff35da7736475fdb0e62c5baded386266029
StevenSun <lifansun1412@gmail.com>
2023-10-08 21:27:34 +0800

    scripts for setup and training

---
+++ tower-detection/setup.sh
@@ -0,0 +1,8 @@
+!#/bin/bash
+
+git clone https://github.com/ultralytics/yolov5  # clone
+cd yolov5
+pip install -r requirements.txt  # install
+cd ..
+curl -L https://app.roboflow.com/ds/1grGMsz4lU?key=OxTMuY2gH7 > roboflow.zip; unzip roboflow.zip; rm roboflow.zip
+cp data.yaml yolov5/data/
