c1da3e331780fae46664606568b4996f063d587e
Lifan Sun <lifansun1412@gmail.com>
2023-11-16 19:07:39 +0800

    Update setup.sh

--- oil-detection/setup.sh
+++ oil-detection/setup.sh
@@ -5,5 +5,5 @@ cd yolov5
 pip install -r requirements.txt  # install
 cd ..
 
-curl -L "https://app.roboflow.com/ds/M2L1dXRjXh?key=8dopNlqTet" &gt; roboflow.zip; unzip roboflow.zip; rm roboflow.zip
+curl -L "https://app.roboflow.com/ds/M2L1dXRjXh?key=8dopNlqTet" > roboflow.zip; unzip roboflow.zip; rm roboflow.zip
 cp data.yaml yolov5/data/
